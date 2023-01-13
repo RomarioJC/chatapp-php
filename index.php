bcf0258e51edccff68a1b222fce655fe05f9871a
RomarioJC <rdcavalo@gmail.com>
2023-01-13 16:04:56 +0100

    Frontend login e signup
    Pagina de login e signup criadas

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="Pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Chat Romário_JC</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <header>Login e Cadastro Chat Romário_JC</header>
+            <form action="#">
+                <div class="error_text">Mensagem de erro</div>
+                <div class="input field">
+                    <label for="nome">Nome</label>
+                    <input type="text" name="nome" id="nome" placeholder="Nome">
+                </div>
+                <div class="input field">
+                    <label for="email">email</label>
+                    <input type="text" name="email" id="email" placeholder="Email">
+                </div>
+                <div class="input field">
+                    <label for="password">Password</label>
+                    <input type="text" name="password" id="password" placeholder="*******">
+                </div>
+                <div class="file field">
+                    <label for="imagem">Selecione uma imagem</label>
+                    <input type="file" name="imagem" id="imagem">
+                </div>
+                <div class="input field">
+                    <input type="submit" value="Continue ">
+                </div>
+            </form>
+            <div class="link">Já é cadastrado? <a href="login.php">Faça o login</a></div>
+        </section>
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="Pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Chat Romário_JC</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <header>Login e Cadastro Chat Romário_JC</header>
+            <form action="#">
+                <div class="error_text">Mensagem de erro</div>
+                <div class="input field">
+                    <label for="email">email</label>
+                    <input type="text" name="email" id="email" placeholder="Email">
+                </div>
+                <div class="input field">
+                    <label for="password">Password</label>
+                    <input type="text" name="password" id="password" placeholder="*******">
+                </div>
+               
+                <div class="input field">
+                    <input type="submit" value="Continue no chat">
+                </div>
+            </form>
+            <div class="link">Ainda não é cadastrado? <a href="index.php">Cadastre-se</a></div>
+        </section>
+    </div>
+</body>
+</html>
